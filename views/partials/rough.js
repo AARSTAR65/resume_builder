ef74ee638e85949785ca03bed7067c2ff6577aa3
aarstar65 <rishav9880@yahoo.com>
2019-02-24 12:27:58 +0530

    commit for deploy to heroku

--- views/partials/rough.js
+++ views/partials/rough.js
@@ -94,3 +94,26 @@
         <%if (LoggedIn) { %>
             <% include ../partials/header-li %>
             <% } %>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World\n');
+}).listen(8081);
+console.log('Server running at http://127.0.0.1:8081/');
